85c673fa2fbd4d008159ccd7c87d0ec0962ee208
rajat-211990 <rajat21.iitd@gmail.com>
2022-05-23 08:29:30 +0530

    new

---
+++ src/DigitButton.js
@@ -0,0 +1,17 @@
+import { ACTIONS } from './App'
+
+export default function DigitButton({dispatch, digit}){
+ return (
+  <button
+   onClick = {()=> dispatch({type:ACTIONS.ADD_DIGIT,payload : { digit }})}
+   
+  >
+    {digit}
+  </button>
+
+
+ )
+
+
+
+}
